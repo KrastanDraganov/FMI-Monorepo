9bed967e7ece5d3f7b376c6a2e790cb5aa83c16b
Krastan Draganov <krastan.draganov@gmail.com>
2025-11-16 08:24:50 +0200

    Add Data Structures Homework 3 problem 1

---
+++ Data-Structures/Homework/03/1.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+
+struct Node
+{
+    char data;
+    Node *next;
+
+    Node(char data, Node *next = nullptr) : data(data), next(next) {}
+};
+
+void print(const Node *list)
+{
+    while (list)
+    {
+        std::cout << list->data;
+
+        if (list->next)
+        {
+            std::cout << " -> ";
+        }
+
+        list = list->next;
+    }
+
+    std::cout << "\n";
+}
+
+void freeList(Node *list)
+{
+    while (list)
+    {
+        Node *toDelete = list;
+        list = list->next;
+
+        delete toDelete;
+    }
+}
+
+bool isLower(char c)
+{
+    return c >= 'a' && c <= 'z';
+}
+
+bool isUpper(char c)
+{
+    return c >= 'A' && c <= 'Z';
+}
+
+bool isDigitChar(char c)
+{
+    return c >= '0' && c <= '9';
+}
+
+Node *transformList(Node *head)
+{
+    Node **curr = &head;
+
+    while (*curr)
+    {
+        Node *node = *curr;
+        char c = node->data;
+
+        if (isLower(c))
+        {
+            Node *upperNode = new Node(c - 'a' + 'A', node);
+            *curr = upperNode;
+
+            curr = &(node->next);
+        }
+        else if (isDigitChar(c))
+        {
+            node->data = '*';
+            curr = &(node->next);
+        }
+        else if (isUpper(c))
+        {
+            curr = &(node->next);
+        }
+        else
+        {
+            *curr = node->next;
+            delete node;
+        }
+    }
+
+    return head;
+}
+
+int main()
+{
+    std::cout << "=== Task 1 ===\n";
+    Node *list1 = new Node('a',
+                           new Node('b',
+                                    new Node('1',
+                                             new Node('-',
+                                                      new Node('C',
+                                                               new Node('@',
+                                                                        new Node('z')))))));
+
+    std::cout << "Original: ";
+    print(list1);
+
+    list1 = transformList(list1);
+
+    std::cout << "Transformed: ";
+    print(list1);
+
+    freeList(list1);
+
+    return 0;
+}
