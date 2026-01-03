eb8df1298800b9f50454f735fe5859434f800f0b
Krastan Draganov <krastan.draganov@gmail.com>
2026-01-03 03:06:59 +0200

    Add Data Structures Homework 05 problem Nuts

---
+++ Data-Structures/Homework/05/nuts.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <unordered_map>
+
+void solve()
+{
+    int x, n;
+    std::cin >> x >> n;
+
+    std::unordered_map<int, int> weightToIndexMap;
+    std::pair<int, int> result = {-1, -1};
+
+    for (int i = 1; i <= n; ++i)
+    {
+        int weight;
+        std::cin >> weight;
+        
+        int target = x - weight;
+
+        if (weightToIndexMap.count(target) > 0)
+        {
+            result = {weightToIndexMap[target], i};
+        }
+        
+        weightToIndexMap[weight] = i;
+    }
+
+    std::cout << result.first << " " << result.second << "\n";
+}
+
+int main()
+{
+    int tests;
+    std::cin >> tests;
+
+    for (int test = 0; test < tests; ++test)
+    {
+        solve();
+    }
+
+    return 0;
+}
